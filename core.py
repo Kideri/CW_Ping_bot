ea30aac6185e2d1f8585b0f66b4a79aa49830a75
Kideri <kiderikun@gmail.com>
2020-07-27 19:28:41 +0300

    Init commit

---
+++ core.py
@@ -0,0 +1,21 @@
+import telebot
+import config
+import ru_en
+from telebot import apihelper
+
+apihelper.proxy = {'https':'https://93.115.23.229:3128'}
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    keyboard = types.KeyboardMarkup()
+    russian = types.keyboardButton(text='\xF0\x9F\x87\xB7\xF0\x9F\x87\xBA Русский', callback_data='ru_Ru')
+    english = types.keyboardButton(test='\xF0\x9F\x87\xBA\xF0\x9F\x87\xB8 English', callback_data='en_US')
+    keyboard.add(russian)
+    keyboard.add(english)
+    bot.send_message(message.chat.id, ru_en.welcome, reply_markup=keyboard)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True, timeout=123)
---
+++ ru_en.py
@@ -0,0 +1,3 @@
+welcome = '''Добро пожаловать в пинг бота для Chat Wars. Для начала выберите язык.
+
+Welcome to ping bot for Chat Wars. To start choose language.'''
