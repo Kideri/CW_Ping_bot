49f421d17afd11cf9657471c1b2035de7cc2439f
Kideri <kiderikun@gmail.com>
2020-07-27 20:00:54 +0300

    Keyboard fix

--- core.py
+++ core.py
@@ -7,9 +7,9 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    keyboard = types.KeyboardMarkup()
-    russian = types.keyboardButton(text='\xF0\x9F\x87\xB7\xF0\x9F\x87\xBA Русский', callback_data='ru_Ru')
-    english = types.keyboardButton(test='\xF0\x9F\x87\xBA\xF0\x9F\x87\xB8 English', callback_data='en_US')
+    keyboard = types.ReplyKeyboardMarkup()
+    russian = types.KeyboardButton(text='\xF0\x9F\x87\xB7\xF0\x9F\x87\xBA Русский', callback_data='ru_Ru')
+    english = types.KeyboardButton(test='\xF0\x9F\x87\xBA\xF0\x9F\x87\xB8 English', callback_data='en_US')
     keyboard.add(russian)
     keyboard.add(english)
     bot.send_message(message.chat.id, ru_en.welcome, reply_markup=keyboard)
