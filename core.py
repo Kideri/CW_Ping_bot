57513f6cdb890679dff916eba4040282750a2634
Kideri <kiderikun@gmail.com>
2020-07-27 20:13:40 +0300

    Layout test

--- core.py
+++ core.py
@@ -10,8 +10,7 @@ def start_command(message):
     keyboard = types.ReplyKeyboardMarkup()
     russian = types.KeyboardButton(text='\xF0\x9F\x87\xB7\xF0\x9F\x87\xBA Русский')
     english = types.KeyboardButton(text='\xF0\x9F\x87\xBA\xF0\x9F\x87\xB8 English')
-    keyboard.add(russian)
-    keyboard.add(english)
+    keyboard.add(russian, english)
     bot.send_message(message.chat.id, ru_en.welcome, reply_markup=keyboard)
 
 
