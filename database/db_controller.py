03a24471bf8fdb41dcb2df47980791f56b3ee71d
Kideri <kiderikun@gmail.com>
2020-08-05 13:46:27 +0300

    Added commiting to DB

--- database/db_controller.py
+++ database/db_controller.py
@@ -5,20 +5,16 @@ from . import db_config as config
 class Controller:
     service = 'DB'
     logger = ''
-    cursor = ''
+    conn = ''
 
     def __init__(self, logger):
         super().__init__()
         self.logger = logger
         self.logger.service_init(self.service)
         self.logger.log(self.service, 'Creating connection')
-        conn = psycopg2.connect(dbname=config.NAME, user=config.USER,
+        self.conn = psycopg2.connect(dbname=config.NAME, user=config.USER,
                                 password=config.PASSWORD, host=config.HOST)
         self.logger.log(self.service, 'Connection created')
-
-        self.logger.log(self.service, 'Creating cursor')
-        self.cursor = conn.cursor()
-        self.logger.log(self.service, 'Cursor created')
 
         self.logger.log(self.service, 'Searching for tables...')
         start_time = time.time()
@@ -26,24 +22,27 @@ class Controller:
         users_result = 'found'
         guilds_table = True
         guilds_result = 'found'
+        cursor = self.conn.cursor()
         try:
-            self.cursor.execute('SELECT * FROM information_schema.tables WHERE table_name=\'%s\''%('users'))
-            if self.cursor.fetchone() is None:
+            cursor.execute('SELECT * FROM information_schema.tables WHERE table_name=\'%s\''%('users'))
+            if cursor.fetchone() is None:
                 users_table = False
                 users_result = 'not found'
         except psycopg2.Error as e:
             users_table = False
             users_result = 'not found'
-            self.cursor.execute('ROLLBACK')
+            cursor.execute('ROLLBACK')
         try:
-            self.cursor.execute('SELECT * FROM information_schema.tables WHERE table_name=\'%s\''%('guilds'))
-            if self.cursor.fetchone() is None:
+            cursor.execute('SELECT * FROM information_schema.tables WHERE table_name=\'%s\''%('guilds'))
+            if cursor.fetchone() is None:
                 guilds_table = False
                 guilds_result = 'not found'
         except psycopg2.Error as e:
             guilds_table = False
             guilds_result = 'not found'
-            self.cursor.execute('ROLLBACK')
+            cursor.execute('ROLLBACK')
+        cursor.close()
+        self.conn.commit()
         self.logger.log(self.service, 'Table users %s, table guilds %s. Time of completion: %sms'%(users_result,
                                                                                                  guilds_result,
                                                                                                  str(int((time.time() - start_time) * 1000))))
@@ -51,7 +50,8 @@ class Controller:
             self.logger.log(self.service, 'Creating not existing tables...')
             start_time = time.time()
             if not users_table:
-                self.cursor.execute('''CREATE TABLE users(
+                cursor = self.conn.cursor()
+                cursor.execute('''CREATE TABLE users(
                                             id serial PRIMARY KEY,
                                             user_id integer NOT NULL,
                                             language text,
@@ -60,14 +60,19 @@ class Controller:
                                             role text,
                                             guild_in_id integer
                                         );''')
+                cursor.close()
+                self.conn.commit()
             if not guilds_table:
-                self.cursor.execute('''CREATE TABLE guilds(
+                cursor = self.conn.cursor()
+                cursor.execute('''CREATE TABLE guilds(
                                             id serial PRIMARY KEY,
                                             chat_id integer,
                                             guild_name text,
                                             guild_admin integer,
                                             ping_time text
                                         );''')
+                cursor.close()
+                self.conn.commit()
             result = ''
             if not users_table and guilds_table:
                 result = 'users, guilds'
@@ -83,15 +88,21 @@ class Controller:
     
 
     def create_user(self, user_id):
-        self.cursor.execute('SELECT * FROM users WHERE user_id = %s', (user_id,))
-        data = self.cursor.fetchone()
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE user_id = %s', (user_id,))
+        data = cursor.fetchone()
         if data is None:
-            self.cursor.execute('INSERT INTO users(user_id) VALUES(%s)', (user_id,))
+            cursor.execute('INSERT INTO users(user_id) VALUES(%s)', (user_id,))
             self.logger.log(self.service, 'User with id {%s} successfully created'%(user_id))
         else:
             self.logger.log(self.service, 'User with id {%s} already exists, nothing happend'%(user_id))
+        cursor.close()
+        self.conn.commit()
     
 
     def set_language(self, user_id, language):
-        self.cursor.execute('UPDATE users SET language=\'%s\' WHERE user_id=%s'%(language, user_id,))
+        cursor = self.conn.cursor()
+        cursor.execute('UPDATE users SET language=\'%s\' WHERE user_id=%s'%(language, user_id,))
         self.logger.log(self.service, 'Language updated in db for user %s to %s language'%(user_id, language))
+        cursor.close()
+        self.conn.commit()
