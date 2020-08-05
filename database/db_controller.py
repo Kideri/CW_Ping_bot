3f3f4a879acac991b98042baa7c7fbe37b805a78
Kideri <kiderikun@gmail.com>
2020-08-05 12:51:57 +0300

    Empty

--- database/db_controller.py
+++ database/db_controller.py
@@ -28,7 +28,6 @@ class Controller:
         guilds_result = 'found'
         try:
             result = self.cursor.execute('SELECT * FROM information_schema.tables WHERE table_name=\'%s\''%('users'))
-            self.cursor.commit()
         except psycopg2.Error as e:
             users_table = False
             users_result = 'not found'
